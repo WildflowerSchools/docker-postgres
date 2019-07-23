c0d21fbdf3842d7aee6d5815affb0702fcf06286
Paul DeCoursey <paul@decoursey.net>
2019-07-23 11:30:17 -0500

    I think this is better

--- restore-entrypoint.sh
+++ restore-entrypoint.sh
@@ -2,6 +2,7 @@
 set -Eeo pipefail
 
 if [[ ! -z "${RESTORE_FROM_BACKUP}" ]]; then
+    mkdir -p "$PGDATA"
     if [ -z "$(ls -A $PGDATA)" ]; then
         echo "restoring from a backup"
         wal-g backup-fetch $PGDATA $RESTORE_FROM_BACKUP
@@ -9,10 +10,12 @@ if [[ ! -z "${RESTORE_FROM_BACKUP}" ]]; then
     else
         echo "$PGDATA not empty, skipping restore"
     fi
+    chown -R postgres "$PGDATA"
+    chmod 700 "$PGDATA"
     cat >> ${PGDATA}/recovery.conf <<EOF
 restore_command = '/usr/local/sbin/restore_command.sh %f %p'
 EOF
-    exec "$@"
-else
-    docker-entrypoint.sh $@
+    chown -R postgres "$PGDATA"
 fi
+
+docker-entrypoint.sh $@
