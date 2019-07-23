052d4841bee0f6ebd6b1f68af5647e7d0db3b821
Paul DeCoursey <paul@decoursey.net>
2019-07-23 09:30:05 -0500

    skip restore if the data dir is not empty

--- restore-entrypoint.sh
+++ restore-entrypoint.sh
@@ -2,9 +2,13 @@
 set -Eeo pipefail
 
 if [[ ! -z "${RESTORE_FROM_BACKUP}" ]]; then
-    echo "restoring from a backup"
-    wal-g backup-fetch $PGDATA $RESTORE_FROM_BACKUP
-    echo "restored"
+    if [ -z "$(ls -A $PGDATA)" ]; then
+        echo "restoring from a backup"
+        wal-g backup-fetch $PGDATA $RESTORE_FROM_BACKUP
+        echo "restored"
+    else
+        echo "$PGDATA not empty, skipping restore"
+    fi
     cat >> ${PGDATA}/recovery.conf <<EOF
 restore_command = '/usr/local/sbin/restore_command.sh %f %p'
 EOF
